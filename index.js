c2f3122695c9bf393ce96c518d0f9d7dc0a01442
STEPHEN BACHER <bacher.stephen@gmail.com>
2024-10-15 11:18:38 +0200

    Push Fictionnal hero

---
+++ index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+console.log(chalk.black("Bruce Wayne"));
+console.log(chalk.redBright("Wade Wilson"));
+console.log(chalk.green("Thomas Anderson"));
+console.log(chalk.red("Peter Parker"));
